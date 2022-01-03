d1b02622b8e23713d4f0daed3449a23e8bc55757
rvalledorjr <richardvjr15@gmail.com>
2022-01-03 11:29:07 +0800

    Added Element plugin

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <div id="app" class="bg-red-600">hello world</div>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+  </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "windi.css";
+import "./plugins/element";
 
 Vue.config.productionTip = false;
 
